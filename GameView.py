e670a583fe7e5e3ca0a3c8826f5b4ab15a735fd3
nicholasmaloney <nickmaster0712@gmail.com>
2023-04-08 12:14:07 -0700

    Merge pull request #6 from gran4/GrantsFORK
    Add atexit, delete Custom Path Finding

--- GameView.py
+++ GameView.py
@@ -1,30 +1,31 @@
 """
-Starting Template
+A-Star Path-finding
 
-Once you have learned how to use classes, you can begin your program with this
-template.
+Artwork from https://kenney.nl
 
 If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.starting_template
+python -m arcade.examples.astar_pathfinding
 """
+
 import arcade
-from player import *
+import random
+
+SPRITE_IMAGE_SIZE = 128
+SPRITE_SCALING = 0.25
+SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Better Move Sprite with Keyboard Example"
-SPRITE_SCALING = 0.5
+SCREEN_TITLE = "A-Star Path-finding"
+
 MOVEMENT_SPEED = 5
 
-SIZE_X = 5000
-SIZE_Y = 5000
+VIEWPORT_MARGIN = 100
+
+
 class MyGame(arcade.Window):
     """
     Main application class.
-
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
     """
 
     def __init__(self, width, height, title):
@@ -37,9 +38,11 @@ class MyGame(arcade.Window):
 
         # Variables that will hold sprite lists
         self.player_list = None
+        self.wall_list = None
+        self.enemy_list = None
 
         # Set up the player info
-        self.player_sprite = None
+        self.player = None
 
         # Track the current state of what key is pressed
         self.left_pressed = False
@@ -47,198 +50,213 @@ class MyGame(arcade.Window):
         self.up_pressed = False
         self.down_pressed = False
 
+        self.physics_engine = None
 
-        self.Enemies = arcade.SpriteList()
-        self.OpenToEnemies = []
-        self.EnemyMap = {}
+        # --- Related to paths
+        # List of points that makes up a path between two points
+        self.path = None
+        # List of points we checked to see if there is a barrier there
+        self.barrier_list = None
 
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
+        # Used in scrolling
+        self.view_bottom = 0
+        self.view_left = 0
+
+        # Set the window background color
+        self.background_color = arcade.color.AMAZON
 
     def setup(self):
         """ Set up the game and initialize the variables. """
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True,
+                                           spatial_hash_cell_size=128)
+        self.enemy_list = arcade.SpriteList()
 
         # Set up the player
-        self.player_sprite = Player(":resources:images/animated_characters/female_person/femalePerson_idle.png",
-                                    SPRITE_SCALING)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
+        resource = ":resources:images/animated_characters/" \
+                   "female_person/femalePerson_idle.png"
+        self.player = arcade.Sprite(resource, SPRITE_SCALING)
+        self.player.center_x = SPRITE_SIZE * 5
+        self.player.center_y = SPRITE_SIZE * 1
+        self.player_list.append(self.player)
+
+        # Set enemies
+        resource = ":resources:images/animated_characters/zombie/zombie_idle.png"
+        enemy = arcade.Sprite(resource, SPRITE_SCALING)
+        enemy.center_x = SPRITE_SIZE * 4
+        enemy.center_y = SPRITE_SIZE * 7
+        self.enemy_list.append(enemy)
+
+        spacing = SPRITE_SIZE * 3
+        for column in range(10):
+            for row in range(15):
+                sprite = arcade.Sprite(":resources:images/tiles/grassCenter.png",
+                                       SPRITE_SCALING)
+
+                x = (column + 1) * spacing
+                y = (row + 1) * sprite.height
+
+                sprite.center_x = x
+                sprite.center_y = y
+                if random.randrange(100) > 30:
+                    self.wall_list.append(sprite)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player,
+                                                         self.wall_list)
+
+        # --- Path related
+        # This variable holds the travel-path. We keep it as an attribute so
+        # we can calculate it in on_update, and draw it in on_draw.
+        self.path = None
+        # Grid size for calculations. The smaller the grid, the longer the time
+        # for calculations. Make sure the grid aligns with the sprite wall grid,
+        # or some openings might be missed.
+        grid_size = SPRITE_SIZE
+
+        # Calculate the playing field size. We can't generate paths outside of
+        # this.
+        playing_field_left_boundary = -SPRITE_SIZE * 2
+        playing_field_right_boundary = SPRITE_SIZE * 35
+        playing_field_top_boundary = SPRITE_SIZE * 17
+        playing_field_bottom_boundary = -SPRITE_SIZE * 2
+
+        # This calculates a list of barriers. By calculating it here in the
+        # init, we are assuming this list does not change. In this example,
+        # our walls don't move, so that is ok. If we want moving barriers (such as
+        # moving platforms or enemies) we need to recalculate. This can be an
+        # time-intensive process depending on the playing field size and grid
+        # resolution.
+
+        # Note: If the enemy sprites are the same size, we only need to calculate
+        # one of these. We do NOT need a different one for each enemy. The sprite
+        # is just used for a size calculation.
+        self.barrier_list = arcade.AStarBarrierList(enemy,
+                                                    self.wall_list,
+                                                    grid_size,
+                                                    playing_field_left_boundary,
+                                                    playing_field_right_boundary,
+                                                    playing_field_bottom_boundary,
+                                                    playing_field_top_boundary)
 
     def on_draw(self):
-        """ Render the screen. """
-
-        # Clear the screen
+        """
+        Render the screen.
+        """
+        # This command has to happen before we start drawing
         self.clear()
 
         # Draw all the sprites.
         self.player_list.draw()
-        self.Enemies.draw()
+        self.wall_list.draw()
+        self.enemy_list.draw()
 
-        # Call draw() on all your sprite lists below
-    def update_player_speed(self):
-
-        # Calculate speed based on the keys pressed
-        self.player_sprite.change_x = 0
-        self.player_sprite.change_y = 0
-
-        if self.up_pressed and not self.down_pressed:
-            self.player_sprite.change_y = MOVEMENT_SPEED
-        elif self.down_pressed and not self.up_pressed:
-            self.player_sprite.change_y = -MOVEMENT_SPEED
-        if self.left_pressed and not self.right_pressed:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
-        elif self.right_pressed and not self.left_pressed:
-            self.player_sprite.change_x = MOVEMENT_SPEED
+        if self.path:
+            arcade.draw_line_strip(self.path, arcade.color.BLUE, 2)
 
     def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        self.player_list.update()
+        """ Movement and game logic """
+
+        # Calculate speed based on the keys pressed
+        self.player.change_x = 0
+        self.player.change_y = 0
+
+        if self.up_pressed and not self.down_pressed:
+            self.player.change_y = MOVEMENT_SPEED
+        elif self.down_pressed and not self.up_pressed:
+            self.player.change_y = -MOVEMENT_SPEED
+        if self.left_pressed and not self.right_pressed:
+            self.player.change_x = -MOVEMENT_SPEED
+        elif self.right_pressed and not self.left_pressed:
+            self.player.change_x = MOVEMENT_SPEED
+
+        # Update the character
+        self.physics_engine.update()
+
+        # Calculate a path to the player
+        enemy = self.enemy_list[0]
+        # Set to True if we can move diagonally. Note that diagonal movement
+        # might cause the enemy to clip corners.
+        self.path = arcade.astar_calculate_path(enemy.position,
+                                                self.player.position,
+                                                self.barrier_list,
+                                                diagonal_movement=False)
+        # print(self.path,"->", self.player.position)
+
+        # --- Manage Scrolling ---
+
+        # Keep track of if we changed the boundary. We don't want to call the
+        # set_viewport command if we didn't change the view port.
+        changed = False
+
+        # Scroll left
+        left_boundary = self.view_left + VIEWPORT_MARGIN
+        if self.player.left < left_boundary:
+            self.view_left -= left_boundary - self.player.left
+            changed = True
+
+        # Scroll right
+        right_boundary = self.view_left + SCREEN_WIDTH - VIEWPORT_MARGIN
+        if self.player.right > right_boundary:
+            self.view_left += self.player.right - right_boundary
+            changed = True
+
+        # Scroll up
+        top_boundary = self.view_bottom + SCREEN_HEIGHT - VIEWPORT_MARGIN
+        if self.player.top > top_boundary:
+            self.view_bottom += self.player.top - top_boundary
+            changed = True
+
+        # Scroll down
+        bottom_boundary = self.view_bottom + VIEWPORT_MARGIN
+        if self.player.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player.bottom
+            changed = True
+
+        # Make sure our boundaries are integer values. While the view port does
+        # support floating point numbers, for this application we want every pixel
+        # in the view port to map directly onto a pixel on the screen. We don't want
+        # any rounding errors.
+        self.view_left = int(self.view_left)
+        self.view_bottom = int(self.view_bottom)
+
+        # If we changed the boundary values, update the view port to match
+        if changed:
+            arcade.set_viewport(self.view_left,
+                                SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                SCREEN_HEIGHT + self.view_bottom)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
 
         if key == arcade.key.UP:
             self.up_pressed = True
-            self.update_player_speed()
         elif key == arcade.key.DOWN:
             self.down_pressed = True
-            self.update_player_speed()
         elif key == arcade.key.LEFT:
             self.left_pressed = True
-            self.update_player_speed()
         elif key == arcade.key.RIGHT:
             self.right_pressed = True
-            self.update_player_speed()
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
 
         if key == arcade.key.UP:
             self.up_pressed = False
-            self.update_player_speed()
         elif key == arcade.key.DOWN:
             self.down_pressed = False
-            self.update_player_speed()
         elif key == arcade.key.LEFT:
             self.left_pressed = False
-            self.update_player_speed()
         elif key == arcade.key.RIGHT:
             self.right_pressed = False
-            self.update_player_speed()
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
-
-    def generateEnemySpawner(self, width, length):
-
-        width *= 50
-        length *= 50
-        self.EnemyMap = {}
-
-        #NOTE: UNCOMMENT everything with SnowMap to visualize where enemies spawn
-        #self.SnowMap = {}
-        self.OpenToEnemies = []
-        x = 0
-        y = 0
-        while x <= width:
-            self.EnemyMap[x] = {}
-            #self.SnowMap[x] = {}
-            while y <= length:
-                self.EnemyMap[x][y] = 0
-                #self.SnowMap[x][y] = 0
-                y += 50
-
-            y = 0
-            x += 50
-    def EnemySpawnPos(self):
-        if len(self.OpenToEnemies) > 0:
-            random_num = random.randrange(0, len(self.OpenToEnemies))
-            return self.OpenToEnemies[random_num][0], self.OpenToEnemies[random_num][1]
-        elif len(self.People) > 0:
-            person = self.People[random.randrange(0, len(self.People))]
-            return person.center_x, person.center_y
-        elif self.population == 0:
-            self.End()
-        #elif len(self.Boats) > 0:
-        #    boat = self.Boats[random.randrange(0, len(self.Boats))]
-        #    return boat.center_x, boat.center_y
-        raise ReferenceError("BUG: Either no People in Spritelist or No place open to enemies")
-    def BuildingChangeEnemySpawner(self, x, y, placing=1, min_dist=100, max_dist= 300):
-        #NOTE: Placing=-1 is for destroying, keep at 1 if placing
-        x = round(x/50)*50
-        y = round(y/50)*50
-
-        for x2 in range(-max_dist, max_dist, 50):
-            if not 0 <= x2+x < SIZE_X:
-                continue
-            for y2 in range(-max_dist, max_dist, 50):
-                if not 0 <= y2+y < SIZE_Y:
-                    continue
-
-                x1 = x2+x
-                y1 = y2+y
-                
-                
-                if abs(x2)<=min_dist and abs(y2)<=min_dist:
-                    self.EnemyMap[x1][y1] -= placing
-                    #self.SnowMap[x1][y1] += placing
-                else:
-                    self.EnemyMap[x1][y1] += placing
-    
-                #NOTE: UPDATE open to Enemies list
-                if self.EnemyMap[x1][y1] > 0:
-                    if not (x1, y1) in self.OpenToEnemies:
-                        self.OpenToEnemies.append((x1, y1))
-                elif (x1, y1) in self.OpenToEnemies:
-                    self.OpenToEnemies.remove((x1, y1))
-                
-                
-                """ 
-                Snow = self.SnowMap[x1][y1]
-
-                land = arcade.get_sprites_at_point((x1, y1), self.Lands)
-                if not land:
-                    pass
-                elif Snow < 1 and land[0].typ == "Snow":
-                    land[0].texture = land[0].prev_texture
-                    land[0].typ = land[0].prev_typ
-                elif Snow >= 1 and land[0].typ != "Snow":
-                    land[0].prev_texture = land[0].texture
-                    land[0].prev_typ = land[0].typ
-                    land[0].typ = "Snow"
-                    #gul-li-ble person
-                    land[0].texture = arcade.load_texture("resources/Sprites/Snow.png")
-                 """
-
 
 
 def main():
     """ Main function """
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
     arcade.run()
 
 
